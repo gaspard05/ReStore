92e50dc779c270a364769f113a126e510836c163
adevlab <adevlab@outlook.com>
2021-10-24 00:11:55 +0100

    End of section 5

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -7,27 +7,25 @@ using API.Entities;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
-namespace API.Controllers
-{
-    [ApiController]
-    [Route("api/[controller]")]
-    public class ProductsController:ControllerBase
-    {
-    private readonly StoreContext _context;
-  
-        public ProductsController(StoreContext context)
-        {
-            _context = context;          
+namespace API.Controllers {
+    public class ProductsController : BaseApiController {
+        private readonly StoreContext _context;
+
+        public ProductsController(StoreContext context) {
+            _context = context;
         }
+
         [HttpGet]
-        public async Task<ActionResult<List<Product>>> GetProducts(){
+        public async Task<ActionResult<List<Product>>> GetProducts() {
             var products = await _context.Products.ToListAsync();
             return Ok(products);
         }
 
         [HttpGet("{id}")]
-        public async Task<ActionResult<Product>>getProduct(int id){
-            return await _context.Products.FindAsync(id);
+        public async Task<ActionResult<Product>> getProduct(int id) {
+            var product = await _context.Products.FindAsync(id);
+            if (product == null) return NotFound();
+            return product;
         }
     }
 }
--- client/src/features/catalog/ProductDetails.tsx
+++ client/src/features/catalog/ProductDetails.tsx
@@ -1,8 +1,10 @@
 import { Divider, Grid, Table, TableBody, TableCell, TableContainer, TableRow, Typography } from '@mui/material';
 import { useParams } from 'react-router-dom';
 import { useEffect, useState } from 'react';
-import axios from 'axios';
 import { Product } from '../../app/models/product';
+import agent from '../../app/api/agent';
+import NotFound from '../../app/errors/NotFound';
+import LoadingComponent from '../../app/layout/LoadingComponent';
 
 export default function ProductDetails() {
   const { id } = useParams<{ id: string }>();
@@ -10,14 +12,16 @@ export default function ProductDetails() {
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    axios.get<Product>(`http://localhost:5000/api/products/${id}`)
-      .then(response => setProduct(response.data))
+    agent.Catalog.details(parseInt(id))
+      .then(response => setProduct(response))
       .catch(error => console.log(error))
       .finally(()=>setLoading(false))
 }, [id])
 
-  if(loading) return <h3>Loading...</h3>
-  if(!product) return <h3>Product id: {id} not found</h3>
+  if (loading) return <LoadingComponent message="Loading Product Detail..." />
+  
+  if (!product) return <NotFound />
+  
   return (
     <Grid container spacing={6}>
       <Grid item xs={6}>
